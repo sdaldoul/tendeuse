db9d9951ea7a7315f8173ba95ad78a8035e67d96
sdaldoul <daldoul.slaheddine@gmail.com>
2019-03-13 11:51:21 +0100

    suppression header file !

--- src/test/java/mow/it/now/core/TendeuseServiceTest.java
+++ src/test/java/mow/it/now/core/TendeuseServiceTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017-2018 by Wordline, an Atos Compagny. All rights reserved. This software is the
- * confidential and proprietary information of Atos ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with Atos. En référence à l’article 17.2 du CCAP du marché
- * n°2016-05, passé entre l’ASIP Santé et la société Worldine et relatif aux Prestations de
- * construction, d’hébergement, d’exploitation des infrastructures téléphoniques, de déploiement
- * technique de la solution SI-Samu et de services téléphoniques, les droits de propriété
- * intellectuelle de Worldline sur ce programme seront cédés à l’ASIP Santé à l’issue du marché,
- * notifié le 5 mai 2017 pour une durée initiale de 5 ans et reconductible une fois pour une période
- * d’un an.
- */
-/**
- * 
- */
 package mow.it.now.core;
 
 import java.util.Arrays;
